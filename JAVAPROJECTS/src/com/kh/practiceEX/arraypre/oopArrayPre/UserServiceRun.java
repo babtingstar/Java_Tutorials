c0b12c080a9ac27dcfda7c4a7fa8882b15200384
이희섭 <magicclawer@gmail.com>
2024-12-09 11:26:36 +0900

    OopArray 객체배열

---
+++ JAVAPROJECTS/src/com/kh/practiceEX/arraypre/oopArrayPre/UserServiceRun.java
@@ -0,0 +1,8 @@
+package com.kh.practiceEX.arraypre.oopArrayPre;
+
+public class UserServiceRun {
+    public static void main(String[] args) {
+        UserService userService = new UserService();
+        userService.setSaveAllUser();
+    }
+}
